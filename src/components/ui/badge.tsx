73ad1c2048df2ceb4bf4724cb252263710a2943c
Matt Goodin <mattgoodin@mac.mynetworksettings.com>
2026-03-15 21:40:21 -0700

    fix badge imports

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,4 +1,5 @@
-import { mergeProps, useRender } from "@base-ui/react"
+import { mergeProps } from "@base-ui/react/merge-props"
+import { useRender } from "@base-ui/react/use-render"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
